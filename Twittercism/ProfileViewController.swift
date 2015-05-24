7a2385e7ceacad497dfddb32148bff3e784fb8f5
Josiah Gaskin <josiahgaskin@gmail.com>
2015-05-24 10:41:48 -0700

    Added login with twitter

--- Twittercism/ProfileViewController.swift
+++ Twittercism/ProfileViewController.swift
@@ -8,7 +8,48 @@
 
 import Foundation
 import UIKit
+import TwitterKit
 
 class ProfileViewController : UIViewController {
+    @IBOutlet weak var loginButtonPlaceholder: UIView!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var loginButton: TWTRLogInButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginButton = TWTRLogInButton(logInCompletion: {
+            (session: TWTRSession!, error: NSError!) in
+            if error != nil {
+                NSLog(error.localizedDescription)
+            } else {
+                self.placeLogoutButton()
+                NSLog("Logged in \(session.userName)!")
+            }
+        })
+        loginButton.center = CGPointMake(self.view.center.x, loginButtonPlaceholder.bounds.size.height / 2);
+        loginButtonPlaceholder.addSubview(loginButton)
+
+        if Twitter.sharedInstance().session() != nil {
+            placeLogoutButton()
+            NSLog("Found account for \(Twitter.sharedInstance().session().userName)")
+        } else {
+            placeLoginButton()
+        }
+    }
+    
+    func placeLogoutButton() {
+        loginButton.hidden = true
+        logoutButton.hidden = false
+    }
+    
+    @IBAction func doLogout(sender: UIButton) {
+        Twitter.sharedInstance().logOut()
+        NSLog("Logged out")
+        placeLoginButton()
+    }
+    
+    func placeLoginButton() {
+        logoutButton.hidden = true
+        loginButton.hidden = false
+    }
 }
